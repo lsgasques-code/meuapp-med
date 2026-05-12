77e993891938a4fede670f5ef73f79b4d06dc1ea
lsgasques-code <lsgasques@gmail.com>
2026-05-11 22:07:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ if "GEMINI_API_KEY" not in st.secrets:
 
 # Configuração da API do Gemini puxando a chave segura
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-model = genai.GenerativeModel('gemini-1.5-pro')
+model = genai.GenerativeModel('gemini-1.5-pro-latest')
 
 # --- BARRA LATERAL ---
 with st.sidebar:
